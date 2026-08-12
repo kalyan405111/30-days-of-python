294f4b8bcbc69f36c81f3bbe2665c6bcc7b8dfb6
Mahesh Babu <maheshbabumallepalli@gmail.com>
2026-08-12 10:56:59 +0530

    Day 2 : Variables

---
+++ Day2/varibles.py
@@ -0,0 +1,51 @@
+first_name,age,price,is_online =["kalyan", 0 ,33.3,False]
+
+print (f"my name is {first_name}")
+print(f"my age is:{age}")
+print (f"the cost is :{price}")
+
+
+if is_online:
+    print("he is online")
+else:
+    print("he is not not online") 
+
+
+
+
+teddy=76      
+if teddy  ==33:
+    print("yes")
+else:
+    print("no")    
+
+age=float(age)
+print(type(age))
+print(age)
+
+student=True
+student=str(student)
+print(type(student))
+print(student)
+
+age=bool(age)
+print(age)
+print(type(age))
+x=2
+y=2.0
+x=x/y
+print(x)
+
+age =int(input("what is your age:"))
+
+age =age+33
+print(f"your age is:{age}")
+
+adjactive=input("enter an adj" )
+noun=input("enter your noun")
+verb=input("enter your  verb")
+adj=input("enter your adj")
+print(f"i saw an {adjactive} youtube channel")
+print(f"the youtube channel name is {noun}")
+print(f"brocode was {verb} a code")
+print(f"i like the {adj}")
