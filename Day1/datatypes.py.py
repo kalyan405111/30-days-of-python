33201c7ef31307f84a5209512e2fd0f914cac8f6
Mahesh Babu <maheshbabumallepalli@gmail.com>
2026-08-12 10:43:52 +0530

    Day 1: Data Types

---
+++ Day1/datatypes.py.py
@@ -0,0 +1,23 @@
+print("Day 1 - 30 Days of Python!")
+print("Hello, Kalyan! Let's go 🚀")
+print("hi")
+print(2+3)
+print(3-1)
+print(3*4)
+print(8/2)
+print(2**3)
+print(10%3)
+print(10//3)
+
+
+
+print(type(38))
+print(type(4.55))
+print(type("hello"))
+print(type([1,2,3,4,5]))
+print(type({3.3,5.4}))
+print(type({"name":"kalyan","age":22}))
+print(type(1+ 3j))
+print({"name":"kalyam"})
+
+
