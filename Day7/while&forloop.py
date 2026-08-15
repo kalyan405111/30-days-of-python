1257b9f0e1730261cc76d62034d82620a2afefc8
Kalyanyalla <157573014+kalyan405111@users.noreply.github.com>
2026-08-15 14:33:25 +0530

    Add files via upload

---
+++ Day7/while&forloop.py
@@ -0,0 +1,98 @@
+#food = input("enter the food you like (gg to quit): ")
+
+#while not food == "gg":
+ #   print(f"you like {food}")
+  #  food = input("enter the food you like (gg to quit): ")
+
+#print("you enter gg so bye bye")
+
+#number = int(input("enter a number (0 to 10):to quit press 5 "))
+#while number >= 0 and number <= 10 and number != 5:
+   # print(f"you enter {number}")
+   # number = int(input("enter a number (0 to 10): "))
+#print("you enter a number less than 0 or greater than 10 so bye bye")
+
+
+principle=0
+rate=0
+time=0
+#while principle <=0:
+   #if principle <=0:
+        #print("principle  should be greater than 0")
+
+#while rate <=0:
+            
+           # rate = float(input("enter the rate: "))
+ #           if rate <=0:
+              #  print("rate should be greater than 0")
+#while time <=0:
+                    
+                  #  time = float(input("enter the time: "))
+ #                   if time <=0:
+                      #  print("time should be greater than 0")
+
+#total= prince *pow((1+ rate  / 100), time)
+#print(round(total,2))
+i=3
+#while i ==3:
+ #   print("hi")
+  #  i+=1
+
+#print("hello")  
+########################################################################################################
+#for i in range(1,4):
+ #   print(i)
+
+#for j in range (1,21):
+ #   if j ==17:
+  #  continue
+# elif j==20:
+ ##     break
+
+   ## else:
+     #  print(j)
+       
+
+#for i in reversed(range(1,11)):
+ #  print(i )
+
+#print("happy new year bro")
+
+#for i in range(1,11,2):
+   #print(i)
+#i=0
+#j=0
+#while i==0:
+  #  while j ==0:
+      # print("hi")
+       #j+=1
+       #i+=1
+#x=3
+#while x==3:
+  #  for y in range(9):
+   #     print("dp")
+    #    x+=1
+
+#for j in range(3):
+ #     for i in range(1,11):
+  #          print(i,end=" ")
+ #     print()      
+#nt("this is called nested loop")     
+
+
+#rows =int(input("enter no of rows:"))
+#coulmn =int(input("enter no of column:"))
+#symbol=input("enter the symbol")
+##   for j in range(coulmn):
+ ##  print()
+
+#import time
+#my_time=int(input("enter your time :"))
+#           sec=my_time%60
+ #           min=int(my_time/60)%60
+  #          hour= int(my_time/3600)
+   #         print(f"{hour:02}:{min:02}:{sec:02}",end="\r ")
+    ##        time.sleep(1)
+      #      my_time-=1     
+#print("time up bro")
+#############################################
